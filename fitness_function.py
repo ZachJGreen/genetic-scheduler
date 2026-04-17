69776040279072c8b1ebed7bd1be29ef78af5a04
ZachJGreen <green.zach.j@gmail.com>
2026-04-17 11:28:43 -0500

    add fitness functions for score calculation and checks

---
+++ fitness_function.py
@@ -0,0 +1,21 @@
+# Issue 2
+def fitness_calculate_final_score():
+    return 0
+
+# Issue 6
+def fitness_facilitator_load_check():
+    return 0
+
+# Issue 5
+def fitness_activity_facilitator_check():
+    return 0
+
+# Issue 4
+def fitness_room_capacity_check():
+    return 0
+
+# Issue 3
+def fitness_time_conflict_check():
+    return 0
+
+
